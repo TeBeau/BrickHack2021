6b9cf2cc8affe475950c83be546fa3e78e6f83ef
jacobgualtieri <jgualtie@icloud.com>
2021-02-20 18:13:09 -0500

    Added basic logic to Places controller

--- Controllers/Places.cs
+++ Controllers/Places.cs
@@ -1,9 +1,32 @@
 using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
 
 namespace helloworld.Controllers
 {
     public class Places
     {
+        private const string API_KEY= "AIzaSyA6OGoQdv_0AqFIh78-RQShkLAUm7EsqEw";
+
         public Places(){}
+
+        public void queryLocation(string code){
+            getdata(code);
+        }
+
+        static async public void getdata(string code)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(string.Format("https://maps.googleapis.com/maps/api/place/autocomplete/json?key="+ API_KEY + "&input=" +code));
+                    Console.WriteLine(response);
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
 }
