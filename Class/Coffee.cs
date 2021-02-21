c502a558e1ffa9cd267b2a0329b94f491c971191
Lauren TeBeau <tebeau12@gmail.com>
2021-02-21 16:21:43 -0500

    fixed the form

--- Class/Coffee.cs
+++ Class/Coffee.cs
@@ -6,23 +6,12 @@ namespace BrickHack2021
     {
         public string name{ get; set; }
         public string description{ get; set; }
-        public Store store{ get; set; }
+        public string store{ get; set; }
         public string recipe{ get; set; }
         public bool saved {get; set;}
 
         public Coffee(){saved=false;}
-        public Coffee( string n, string d, Store s){
-            name = n;
-            description =d;
-            store = s;   
-            saved= false;
-        }
-        public Coffee( string n, string d,string rec){
-            name = n;
-            description =d;
-            recipe= rec; 
-            saved =false;
-        }
+
         
     }
 }
